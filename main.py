51b852e48d78b8976d067a5e76d4fc5ce1a10241
kaiyichen <77236381+kaiyichen@users.noreply.github.com>
2022-01-08 04:09:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -299,7 +299,11 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://easyfaces.herokuapp.com/' + TOKEN)
+
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
